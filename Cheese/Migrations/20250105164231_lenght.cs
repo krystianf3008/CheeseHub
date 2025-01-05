823ce8c3d75ff4924ef14e7ca34b47b534d69510
Krystian Frąckowiak <frackowiak.krystian@o2.pl>
2025-01-05 17:42:56 +0100

    maxlenghts

---
+++ Cheese/Migrations/20250105164231_lenght.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheeseHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class lenght : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Category",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Category",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
--- Cheese/Models/Comment/Comment.cs
+++ Cheese/Models/Comment/Comment.cs
@@ -1,4 +1,6 @@
-﻿namespace CheeseHub.Models.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheeseHub.Models.Comment
 {
     public class Comment
     {
@@ -6,10 +8,10 @@
         public Guid VideoId { get; set; }
         public Guid UserId { get; set; }
         public virtual User.User User { get; set; }
+        [MaxLength(500)]
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         public Guid? ParentCommentId { get; set; } 
-
         public Video.Video Video { get; set; }
         public virtual Comment ParentComment { get; set; }
         public ICollection<Comment> Replies { get; set; }
